f968682d7670c364edfdcaa12e74399d91a28940
Roman Gonyukov <roman@bx24.store>
2020-08-07 16:46:36 +0700

    rename trait alias

--- src/classes/crm/company/userfield.php
+++ src/classes/crm/company/userfield.php
@@ -6,11 +6,11 @@ namespace Bitrix24\CRM\Company;
 
 use Bitrix24\Bitrix24Entity;
 use Bitrix24\Contracts\iBitrix24;
-use Bitrix24\Traits\CRM\UserField as ufTrait;
+use Bitrix24\Traits\CRM\UserField as ufCrmTrait;
 
 class UserField extends Bitrix24Entity
 {
-    use ufTrait;
+    use ufCrmTrait;
 
     public function __construct(iBitrix24 $client)
     {
--- src/classes/crm/contact/userfield.php
+++ src/classes/crm/contact/userfield.php
@@ -4,14 +4,14 @@ namespace Bitrix24\CRM\Contact;
 
 use Bitrix24\Bitrix24Entity;
 use Bitrix24\Contracts\iBitrix24;
-use Bitrix24\Traits\CRM\UserField as ufTrait;
+use Bitrix24\Traits\CRM\UserField as ufCrmTrait;
 
 /**
  * Class UserField
  */
 class UserField extends Bitrix24Entity
 {
-	use ufTrait;
+	use ufCrmTrait;
 
 	public function __construct(iBitrix24 $client)
     {
