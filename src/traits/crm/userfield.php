c51abafd3c90ed4835f0c8abcfc5a8ed5971b4bc
Roman Gonyukov <roman@bx24.store>
2020-08-07 15:51:18 +0700

    add method setEntity

--- src/traits/crm/userfield.php
+++ src/traits/crm/userfield.php
@@ -6,5 +6,16 @@ namespace Bitrix24\Traits\CRM;
 
 trait UserField
 {
+    /**
+     * @var string $entity
+     */
+    public $entity;
 
+    /**
+     * @param string $entity
+     */
+    public function setEntity(string $entity): void
+    {
+        $this->entity = $entity;
+    }
 }
