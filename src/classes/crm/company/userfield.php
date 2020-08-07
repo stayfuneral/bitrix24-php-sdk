93305af71d4c4150e4dc041dc2cc3f90bc7f2179
Roman Gonyukov <roman@bx24.store>
2020-08-07 16:39:38 +0700

    add company userfields class

---
+++ src/classes/crm/company/userfield.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Bitrix24\CRM\Company;
+
+
+use Bitrix24\Bitrix24Entity;
+use Bitrix24\Contracts\iBitrix24;
+use Bitrix24\Traits\CRM\UserField as ufTrait;
+
+class UserField extends Bitrix24Entity
+{
+    use ufTrait;
+
+    public function __construct(iBitrix24 $client)
+    {
+        $this->setEntity('company');
+        parent::__construct($client);
+    }
+
+}
