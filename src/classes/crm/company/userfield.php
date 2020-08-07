f3373ee376956bfb8a07792bcf05caf135260d64
Roman Gonyukov <roman@bx24.store>
2020-08-07 18:41:45 +0700

    update crm userfields classes:
    1. lead
    2. contact
    3. company
    4. deal
    5. invoice
    6. requisite

--- src/classes/crm/company/userfield.php
+++ src/classes/crm/company/userfield.php
@@ -6,11 +6,11 @@ namespace Bitrix24\CRM\Company;
 
 use Bitrix24\Bitrix24Entity;
 use Bitrix24\Contracts\iBitrix24;
-use Bitrix24\Traits\CRM\UserField as ufCrmTrait;
+use Bitrix24\Traits\CRM\UfCrmTrait;
 
 class UserField extends Bitrix24Entity
 {
-    use ufCrmTrait;
+    use UfCrmTrait;
 
     public function __construct(iBitrix24 $client)
     {
--- src/classes/crm/contact/userfield.php
+++ src/classes/crm/contact/userfield.php
@@ -4,14 +4,14 @@ namespace Bitrix24\CRM\Contact;
 
 use Bitrix24\Bitrix24Entity;
 use Bitrix24\Contracts\iBitrix24;
-use Bitrix24\Traits\CRM\UserField as ufCrmTrait;
+use Bitrix24\Traits\CRM\UfCrmTrait;
 
 /**
  * Class UserField
  */
 class UserField extends Bitrix24Entity
 {
-	use ufCrmTrait;
+	use UfCrmTrait;
 
 	public function __construct(iBitrix24 $client)
     {
